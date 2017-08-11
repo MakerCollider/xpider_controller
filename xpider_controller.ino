df71d1fab4a68d162e8c88aa00a190f8c861580a
Ye Tian <flymaxty@foxmail.com>
2017-08-11 22:22:15 +0800

    add wifi status led, add controller version on inside_protocol

--- xpider_controller.ino
+++ xpider_controller.ino
@@ -18,15 +18,13 @@
 
 #include "Arduhdlc.h"
 
-#include "task.h"
-
 #include "xpider_task.h"
 #include "xpider_pin.h"
 #include "xpider_imu.h"
 #include "xpider_control.h"
 #include "xpider_inside_protocol.h"
 
-unsigned long start_time1, start_time2;
+#define CONTROLLER_VERSION "3.1.0"
 
 Arduhdlc *g_hdlc;
 XpiderIMU *g_xpider_imu;
@@ -80,9 +78,44 @@ void SetFrontLeds(const uint8_t leds[6]) {
   g_xpider_control->SetFrontLeds(leds);
 }
 
-void setup() {
-  Serial.begin(9600);
+void GetRegister(XpiderInsideProtocol::RegisterIndex register_index) {
+  switch(register_index) {
+    case XpiderInsideProtocol::kControllerVersion: {
+      uint8_t led_a[6] = {20, 20, 20, 0, 20, 20};
+      g_xpider_control->SetFrontLeds(led_a);
+      String temp = CONTROLLER_VERSION;
+      g_xpider_inside_protocol.RegisterResponse(register_index, temp.c_str(), temp.length()+1);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
 
+void WaitNetWithBlink() {
+  bool rak_starting = true;
+  uint8_t led_a[6] = {0, 20, 0, 0, 20, 0};
+  uint8_t led_b[6] = {0, 0, 20, 0, 0, 20};
+  uint8_t led_off[6] = {0, 0, 0, 0, 0, 0};
+  unsigned long start_time = millis();
+
+  while(rak_starting) {
+    if(digitalRead(CAMERA_READY)) {
+      g_xpider_control->SetFrontLeds(led_a);
+      if(millis() - start_time > 3000){
+        rak_starting = false;
+      }
+    } else {
+      g_xpider_control->SetFrontLeds(led_b);
+      start_time = millis();
+    }
+  }
+
+  g_xpider_control->SetFrontLeds(led_off);
+}
+
+void setup() {
   g_hdlc = new Arduhdlc(&sendframe, &frameHandler, 256);
 
   g_xpider_imu = XpiderIMU::instance();
@@ -98,24 +131,17 @@ void setup() {
   callback_list.set_eye = &SetEye;
   callback_list.auto_move = &SetAutoMove;
   callback_list.set_front_leds = &SetFrontLeds;
+  callback_list.get_register = &GetRegister;
   g_xpider_inside_protocol.Initialize(&InsideSendData, callback_list);
 
-  TaskInitialize(&g_xpider_inside_protocol, g_xpider_control, g_xpider_imu, g_hdlc); 
+  TaskInitialize(&g_xpider_inside_protocol, g_xpider_control, g_xpider_imu, g_hdlc);
 
-  // start_time1 = millis();
-  // delay(2000);
-  // start_time2 = millis();
+  WaitNetWithBlink();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // g_xpider_control->Walk(70);
-  // delay(2000);
-  // delay(1000);
-  // SetRotate(90);
-  // delay(1000);
-  // g_xpider_control->StopRotating();
-  // delay(1000);
-
   /* 
    * Execute each task with defined period
    */
